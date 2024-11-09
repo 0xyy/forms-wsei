8ded40cc0a9d79e7eb90b5ee54c2a8f111bb5a27
Tommy Phan <tommyphan2004.tp@gmail.com>
2024-11-09 13:54:11 +0100

    first commit

---
+++ Forms/Controllers/UserProfileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Forms.Models;
+
+public class UserProfileController : Controller
+{
+    [HttpGet]
+    public IActionResult Form()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Form(UserProfile userProfile)
+    {
+        if (ModelState.IsValid)
+        {
+            return View("Result", userProfile);
+        }
+        return View();
+    }
+
+    public IActionResult Result(UserProfile userProfile)
+    {
+        return View(userProfile);
+    }
+}
---
+++ Forms/Models/UserProfile.cs
@@ -0,0 +1,41 @@
+﻿namespace Forms.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class UserProfile
+    {
+        [Required(ErrorMessage = "Please enter your first name")]
+        [MinLength(2, ErrorMessage = "First name must be at least 2 characters long")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [MinLength(2, ErrorMessage = "Last name must be at least 2 characters long")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Please provide an email address")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string? EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Please create a password")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, and one number")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string? ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [Phone(ErrorMessage = "Invalid phone number format")]
+        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter your age")]
+        [Range(18, 65, ErrorMessage = "Age must be between 18 and 65")]
+        public int? Age { get; set; }
+
+        [Required(ErrorMessage = "Please select a city")]
+        public string? City { get; set; }
+
+        public enum Cities { NewYork, LosAngeles, Chicago, Houston, Miami }
+    }
+}
